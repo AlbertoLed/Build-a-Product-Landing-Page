336a92de89cdcbe1154b5af9c820fe6ea4f8d85a
Alberto Ledesma <alberto.ledesma.dev@gmail.com>
2023-05-31 17:41:12 -0600

    comments in main.js

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const navMobileBtn = document.getElementById('nav-mobile-btn');
 const navMobile = document.getElementById('nav-mobile');
 
+//When the nav bar icon is clicked then shows navbar
 navMobileBtn.addEventListener('click', () => navMobile.classList.contains('hidden') ? navMobile.classList.remove('hidden') : navMobile.classList.add('hidden'));
 
+//When and option in the navbar is clicked then hidden navbar
 navMobile.addEventListener('click', (e) => {
     if(e.target.matches('#nav-mob-features') || e.target.matches('#nav-mob-overview')) {
         navMobile.classList.add('hidden');
